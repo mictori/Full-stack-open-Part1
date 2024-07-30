eed45fdfd34fd8d97e0353107296de3513decd06
mictori <mmictori@gmail.com>
2024-07-30 15:13:42 +0200

    Step 6 add statistics table

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -5,12 +5,16 @@ const Statistics = (props) => {
     return (
       <div>
         <h1>Statistics</h1>
-        <StatisticsLine text='Good' value={props.good} />
-        <StatisticsLine text='Neutral' value={props.neutral}/>
-        <StatisticsLine text='Bad' value={props.bad}/>
-        <StatisticsLine text='All' value={props.all}/>
-        <StatisticsLine text='Average' value={props.average ? props.average : 0}/> 
-        <StatisticsLine text='Positive' value={props.positive ? props.positive : 0}/>
+        <table>
+          <tbody>
+            <StatisticsLine text='Good' value={props.good} />
+            <StatisticsLine text='Neutral' value={props.neutral}/>
+            <StatisticsLine text='Bad' value={props.bad}/>
+            <StatisticsLine text='All' value={props.all}/>
+            <StatisticsLine text='Average' value={props.average}/> 
+            <StatisticsLine text='Positive' value={props.positive + ' %'}/>
+          </tbody>
+        </table>
       </div>
     )
   }
--- src/Components/StatisticsLine.jsx
+++ src/Components/StatisticsLine.jsx
@@ -1,10 +1,13 @@
 const StatisticsLine = (props) => {
   return (
-    <div>
-        <p>
-            {props.text} {props.value}
-        </p>
-    </div>
+    <tr>
+        <td>
+            {props.text} 
+        </td>
+        <td>
+            {props.value}         
+        </td>
+    </tr>
   )
 }
 
