57d3c0b117d7019bed168ce65a003b5dfe037e9b
mictori <mmictori@gmail.com>
2024-07-30 15:02:09 +0200

    Step 4 add conditinal rendering statisctics

--- src/Components/Statistics.jsx
+++ src/Components/Statistics.jsx
@@ -1,17 +1,27 @@
 import StatisticsLine from "./StatisticsLine"
 
 const Statistics = (props) => {
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <StatisticsLine text='Good' value={props.good} />
-      <StatisticsLine text='Neutral' value={props.neutral}/>
-      <StatisticsLine text='Bad' value={props.bad}/>
-      <StatisticsLine text='All' value={props.all}/>
-      <StatisticsLine text='Average' value={props.average ? props.average : 0}/> 
-      <StatisticsLine text='Positive' value={props.positive ? props.positive : 0}/>
-    </div>
-  )
+  if (props.all > 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <StatisticsLine text='Good' value={props.good} />
+        <StatisticsLine text='Neutral' value={props.neutral}/>
+        <StatisticsLine text='Bad' value={props.bad}/>
+        <StatisticsLine text='All' value={props.all}/>
+        <StatisticsLine text='Average' value={props.average ? props.average : 0}/> 
+        <StatisticsLine text='Positive' value={props.positive ? props.positive : 0}/>
+      </div>
+    )
+  }
+  {
+    return(
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedback given</p>
+      </div>
+    )
+  }
 }
 
 export default Statistics
