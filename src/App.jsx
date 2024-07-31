d9625e13c710c6a8b44a6c709eafe7928d3d6682
mictori <mmictori@gmail.com>
2024-07-31 09:15:23 +0200

    Part 1 anecdotes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from "react";
-import Feedback from "./Components/Feedback";
-import Statistics from "./Components/Statistics";
+import { useState } from "react"
 
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+  //index of anecdotes
+  const [selected, setSelected] = useState(0)
 
-  const totalNumber = good + neutral + bad;
-  const averageNumber = (good * 1 + bad * -1) / totalNumber;
-  const positiveNumber = (good * 100) / totalNumber;
+  function getNextAnecdote() {
+    const randomNumber = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomNumber)
+  }
 
-  function increaseByOne(setCount, count) {
-    setCount(count + 1)
+  const points = Array(anecdotes.length).fill(0)
+  const [votes, setVotes] = useState(points)
+
+  function countVotes() {
+    const votesCopy = [...votes]
+    votesCopy[selected] += 1
+    setVotes(votesCopy)
+    getMostVotes()
+  }
+
+  const [mostVotes, setMostVotes] = useState()
+
+  function getMostVotes() {
+    const maxVotesIndex = votes.indexOf(Math.max(...votes))
+    setMostVotes(anecdotes[maxVotesIndex])
   }
 
   return (
     <div>
-      <Feedback 
-        onClickGood={() => increaseByOne(setGood, good)}
-        onClickNeutral={() => increaseByOne(setNeutral, neutral)}
-        onClickBad={() => increaseByOne(setBad, bad)}
-      />
-      <Statistics 
-        good={good}
-        neutral={neutral}
-        bad={bad}
-        all={totalNumber}
-        average={averageNumber}
-        positive={positiveNumber}
-      />
+      <h1>Anecdote of the day</h1>
+      <h3>{anecdotes[selected]}</h3>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={countVotes}>vote</button>
+      <button onClick={getNextAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <p>{mostVotes}</p>
     </div>
   )
 }
